3d5e9d0b074241153b728d712691d393d95a13b0
Loris <45939615+lchastanet@users.noreply.github.com>
2023-03-23 10:28:28 +0100

    try 404 redirect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/item-list" element={<ItemList />} />
-        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
